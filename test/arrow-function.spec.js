2e95dd5f50a35d87939843eeb7438dac714c886b
Boubacar S. BARRY <bouba@hackages.io>
2016-11-04 09:10:19 +0100

    Add koans when arrow-function is invoked through call or apply

--- test/arrow-function.spec.js
+++ test/arrow-function.spec.js
@@ -1,12 +1,61 @@
-import chai from 'chai';
-chai.should();
+import { expect, should } from 'chai';
+should();
 
 // You can implement your solution in another file or inline here
 
 // You can find more information about arrow-function mdn by following the link below
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 
+
 describe('Arrow function', () => {
+  describe('Invoked through call or apply', () => {
+    it('Should not bind this through call', () => {
+      const person = {
+        age: 1,
+
+        add: function (age) {
+          const f = v => v + this.age;
+          return f(age);
+        },
+
+        addThruCall: function (age) {
+          let f = v => {
+            return v + this.age;
+          };
+
+          const hidden = {
+            age: 2
+          };
+
+          return f.call(hidden, age);
+        },
+        addThruApply: function (age) {
+          let f = v => {
+            return v + this.age;
+          };
+
+          const hidden = {
+            age: 2
+          };
+
+          return f.apply(hidden, [age]);
+        }
+      };
+
+      let actual = person.add(1);
+
+      expect(actual).equal(__);
+
+      actual = person.addThruCall(1);
+
+      expect(actual).equal(__);
+
+      actual = person.addThruApply(1);
+
+      expect(actual).equal(__);
+    });
+
+  });
   describe('Add or Sum function...', () => {
     it('Should return 5 when passing 2 and 3', () => {
       add(2, 3).should.equal(5);
@@ -14,7 +63,7 @@ describe('Arrow function', () => {
 
     it('Should throw an error if only one argument is passed', () => {
       (() => {
-        add(2)
+        add(2);
       }).should.throw();
     });
 
@@ -34,9 +83,9 @@ describe('Arrow function', () => {
       var cart = {
         owner: 'Davy',
         fruits: ['apple', 'brussels sprout', 'banana', 'mango'],
-        printFruits: function() {
-          this.fruits.forEach(function(f) {
-            console.log(this.owner + " bought " + f)
+        printFruits: function () {
+          this.fruits.forEach(function (f) {
+            console.log(this.owner + " bought " + f);
           });
         }
       };
