1429438b0da8c40ab8e2abd9098fb2fef89189b7
Boubacar S. BARRY <bouba@hackages.io>
2016-11-07 13:33:27 +0100

    Add babel polyfill  and remove iterator test case

--- test/generators.spec.js
+++ test/generators.spec.js
@@ -1,18 +1,6 @@
-import {expect} from 'chai';
+import 'babel-polyfill';
+import { expect } from 'chai';
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
 // http://koajs.com/
 
-class Company {
-  constructor(customers){
-    this.customers = customers;
-  }
-}
-
-describe('Generators', ()=>{
-  it('Company should be iterable by customers', ()=>{
-    let company = new Company(['Davy', 'Thomas', 'Lars']);
-    // TODO Fix the line below to extract the list of customers from the Company instance.
-    expect([...company]).to.deep.equal(['Davy', 'Thomas', 'Lars']);
-  });
-});
