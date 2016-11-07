6695748e0be31d553c6d943c07828e4d13a19060
Boubacar S. BARRY <bouba@hackages.io>
2016-11-07 09:16:01 +0100

    Reorganise class koans + add hoisting test case

--- test/classes.spec.js
+++ test/classes.spec.js
@@ -5,11 +5,53 @@ chai.should();
 // You can implement your solution in another file or inline here
 
 describe('Class in ES6', () => {
-  describe('Foo class', () => {
-
+  describe('Like a function ...', () => {
+    // Declare the Human class below that will satisfy all assertions
+    let Human;
     it('should be a function', () => {
+
       expect(Human).to.be.a('function');
     });
+    it('should not be hoisted like function', () => {
+      let polyFunc;
+
+      (() => {
+        polyFunc = new PolygoneFunc(3, 5);
+      }).should.not.throw(Error);
+
+      function PolygoneFunc(height, width) {
+        this.height = height;
+        this.width = width;
+
+        this.log = function () {
+          return `H:${this.height} & W:${this.width}`;
+        };
+      }
+
+      expect(polyFunc.log()).equal('H:3 & W:5');
+
+      let polyClass;
+      
+      // Hint: the declaration position of PolygoneClass is not correct
+      (() => {
+        polyClass = new PolygoneClass(3, 5);
+      }).should.not.throw(Error);
+
+      class PolygoneClass {
+        constructor(height, width) {
+          this.height = height;
+          this.width = width;
+        }
+        log() {
+          return `H:${this.height} & W:${this.width}`;
+        }
+      }
+
+      expect(polyClass.log()).equal('H:3 & W:5');
+
+    });
+  });
+  describe('Property ...', () => {
 
     it('should have a name property called Human', () => {
       expect(Human).has.property('name').equal('Human');
@@ -54,3 +96,4 @@ describe('Class in ES6', () => {
     });
   });
 });
+
