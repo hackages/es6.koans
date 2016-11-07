7ea829e1fbac2b6cc35a2b92fb753eb6448682cd
Boubacar S. BARRY <bouba@hackages.io>
2016-11-07 12:35:42 +0100

    Add iterator protocol

--- test/iterators.spec.js
+++ test/iterators.spec.js
@@ -23,32 +23,36 @@ describe('iterator function', ()=>{
         .to.be.a('function');
     });
   });
-  
-  it('Should return an object that provides a next method/function', ()=>{
 
-    expect(iterator([]))
-      .to.be.an('object')
-      .that.has.property('next')
-      .to.be.a('function');
+  describe('Iterator Protocol ...', () => {
+    it('Should return an object that provides a next method/function', () => {
+      // Implement a iterator below to satisfy all assertions
+      let iterator;
 
+      expect(iterator([]))
+        .to.be.an('object')
+        .that.has.property('next')
+        .to.be.a('function');
 
-    describe('next function', ()=>{
-      it('Should return an object with two properties: done of type boolean and value', () => {
-        let returnObject = iterator([]).next();
-        expect(returnObject).to.have.a.property('value').to.equal(undefined);
-        expect(returnObject).to.have.a.property('done').to.be.a('boolean').to.equal(true);
-      });
+      describe('next function', () => {
+        it('Should return an object with two properties: done of type boolean and value', () => {
+          let returnObject = iterator([]).next();
+          expect(returnObject).to.have.a.property('value').to.equal(undefined);
+          expect(returnObject).to.have.a.property('done').to.be.a('boolean').to.equal(true);
+        });
 
-      it('Should return the next item in the collection', ()=>{
-        let items = iterator(['Maartens', 'Johan', 'Eric', 'Tom']);
+        it('Should return the next item in the collection', () => {
+          let items = iterator(['Maartens', 'Johan', 'Eric', 'Tom']);
 
-        expect(items.next()).to.deep.equal({value: 'Maartens', done: false});
-        expect(items.next()).to.deep.equal({value: 'Johan', done: false});
-        expect(items.next()).to.deep.equal({value: 'Eric', done: false});
-        expect(items.next()).to.deep.equal({value: 'Tom', done: false});
-        expect(items.next()).to.deep.equal({value: undefined, done: true});
-        expect(items.next()).to.deep.equal({value: undefined, done: true});
+          expect(items.next()).to.deep.equal({ value: 'Maartens', done: false });
+          expect(items.next()).to.deep.equal({ value: 'Johan', done: false });
+          expect(items.next()).to.deep.equal({ value: 'Eric', done: false });
+          expect(items.next()).to.deep.equal({ value: 'Tom', done: false });
+          expect(items.next()).to.deep.equal({ value: undefined, done: true });
+          expect(items.next()).to.deep.equal({ value: undefined, done: true });
+        });
       });
     });
   });
+
 });
