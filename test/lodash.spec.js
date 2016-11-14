3803f9abe19e4f3497e1cca99566dcf17eb4a5c9
Boubacar S. BARRY <bouba@hackages.io>
2016-11-14 11:24:50 +0100

    Remove lodash ordering and refractor import

--- test/lodash.spec.js
+++ test/lodash.spec.js
@@ -3,7 +3,7 @@ import sinon from 'sinon';
 const expect = chai.expect;
 chai.should();
 
-import * as _ from '../src/lodash/index';
+import * as _ from '../src/lodash/';
 
 describe('Underscore library', () => {
 
